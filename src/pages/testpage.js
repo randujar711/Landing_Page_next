de9769fceec6962a6ae13d8726ad93ba1eeaa6a1
Ryan Andujar <randujar711@gmail.com>
2023-03-18 16:40:15 -0400

    initial

---
+++ src/pages/testpage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'next/link'
+// import Home from '../styles/Home.modules.css'
+
+export default function testpage() {
+  return (
+    <div >
+        <h1>testpage</h1>
+        <Link href="/">go home</Link>
+    </div>
+  )
+}

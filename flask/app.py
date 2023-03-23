89492c2988362279d41a05e9264f117559326593
Ryan Andujar <97029985+randujar711@users.noreply.github.com>
2023-03-23 11:28:26 -0400

    Add files via upload

---
+++ flask/app.py
@@ -0,0 +1,69 @@
+import os
+from flask import Flask, send_file, request, jsonify
+from flask_migrate import Migrate
+from flask_cors import CORS
+from config import Config
+from models import db, Projects
+from pprint import pprint
+from flask_socketio import SocketIO, emit
+import platform
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, JWTManager
+
+app = Flask(__name__, static_folder='public')
+CORS(app, origins=['*'])
+app.config.from_object(Config)
+jwt = JWTManager(app)
+db.init_app(app)
+migrate = Migrate(app, db)
+socketio = SocketIO(app, cors_allowed_origins='*')
+
+# In Rails, controller actions and routes are separate
+# Here in Flask, they are put together
+
+@app.get('/')
+def home():
+    return send_file('welcome.html')
+
+
+@app.get('/example')
+def example():
+    return {'message': 'Your app is running Python'}
+
+
+@app.get('/info')
+def info():
+    print(dir(platform))
+    return {'machine': platform.node()}
+
+
+@app.get('/projects')
+def projects():
+    projects = Projects.query.all()
+    project_list = [project.to_dict for project in projects]
+    return jsonify(project_list), 201
+
+
+
+@socketio.on('connect')
+def connected():
+    '''This function is an event listener that gets called when the client connects to the server'''
+    print(f'Client {request.sid} has connected')
+    emit('connect', {'data': f'id: {request.sid} is connected'})
+
+
+@socketio.on('data')
+def handle_message(data):
+    '''This function runs whenever a client sends a socket message to be broadcast'''
+    print(f'Message from Client {request.sid} : ', data)
+    emit('data', {'data': 'data', 'id': request.sid}, broadcast=True)
+
+
+@socketio.on("disconnect")
+def disconnected():
+    '''This function is an event listener that gets called when the client disconnects from the server'''
+    print(f'Client {request.sid} has disconnected')
+    emit('disconnect', f'Client {request.sid} has disconnected', broadcast=True)
+
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=os.environ.get('PORT', 5000))
---
+++ flask/models.py
@@ -0,0 +1,36 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy() # create an instance of a database connection
+migrate = Migrate(db) # associate migration functions to it
+
+# This ORM has the migration and the model together
+class Projects(db.Model):
+    # This is the migration part
+    __tablename__ = 'projects'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), unique=True, nullable=False)
+    desc = db.Column(db.Text, unique=False, nullable=True )
+    stack = db.Column(db.String, unique=False, nullable=True)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+
+    # This is regular old Python classes
+    # Right here is where we "whitelist" what can be set when creating a user
+    # any column omitted cannot be set by the user/app manually
+    def __init__(self, title, desc, stack):
+        self.title = title
+        self.desc = desc
+        self.stack = stack 
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'desc': self.desc,
+            'stack': self.stack,
+            'created_at': self.created_at
+        }
+
+    def __repr__(self):
+        return '<Projects %r>' % self.title
